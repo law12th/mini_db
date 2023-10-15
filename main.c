0af1a44f88f9d6c7dca5388d049299aaecdb9db7
law12th <lawrenctepula@gmail.com>
2023-10-15 19:47:50 +0200

    chore: break program if args less than 3

--- main.c
+++ main.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 #include "password.h"
 #include "database.h"
+#include "utils.h"
 
 int main(int argc, char **argv)
 {
+    if (argc < 3)
+        die("usage: mini_db <dbfile> <action> [action params]");
+
     return EXIT_SUCCESS;
 }
